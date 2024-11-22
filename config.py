1547c1df81439d3a999a21f2a6b4f1220476fc2a
kawukiandrew7777 <60316867+kawukiandrew7777@users.noreply.github.com>
2024-11-22 23:41:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 
 # Auto delete time in seconds.
 AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "0"))
-AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "This file will be automatically deleted in {time} hour. Please ensure you have saved any necessary content before this time.")
+AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "<b>REMINDER:</b>/n<i>These files will be automatically deleted in <b>{time} hour</b>. Please ensure you have saved any necessary content before this time.</i>")
 AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
 
 #Set true if you want Disable your Channel Posts Share button
