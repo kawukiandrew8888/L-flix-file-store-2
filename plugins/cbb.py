6c95c03a689ced5a0d2e9308a769d0e1e2e4bb83
kawukiandrew7777 <60316867+kawukiandrew7777@users.noreply.github.com>
2024-11-24 21:17:31 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @katandika2022\n○ Support Group : @katandika2023</b>",
+            text = f"<b> This bot was created by 👉 <a href='https://t.me/lflixadmin'>L-FLIX Admin</a>, If you need a similar bot like this, you can contact the admin on <code>+256704312951</code></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
