341698903722859f46e57fef4636349e67b824a7
kawukiandrew7777 <60316867+kawukiandrew7777@users.noreply.github.com>
2024-11-24 23:07:19 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b> This bot was created by 👉 <a href='https://t.me/lflixadmin'>L-FLIX Admin</a> /n 𝑰𝒇 𝒚𝒐𝒖 𝒏𝒆𝒆𝒅 𝒂 𝒔𝒊𝒎𝒊𝒍𝒂𝒓 𝒃𝒐𝒕 𝒍𝒊𝒌𝒆 𝒕𝒉𝒊𝒔, 𝒚𝒐𝒖 𝒄𝒂𝒏 𝒄𝒐𝒏𝒕𝒂𝒄𝒕 𝒕𝒉𝒆 𝒂𝒅𝒎𝒊𝒏 𝒐𝒏:/n/n  𝑷𝒉𝒐𝒏𝒆 𝒏𝒐:<code>+256704312951</code>/n/n 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑:<a href='https://wa.me/qr/A3GNDFJZU2G3P1'>𝑪𝑳𝑰𝑪𝑲 𝑯𝑬𝑹𝑬</a></b>",
+            text = f"<b> This bot was created by 👉 <a href='https://t.me/lflixadmin'>L-FLIX Admin</a> /n 𝑰𝒇 𝒚𝒐𝒖 𝒏𝒆𝒆𝒅 𝒂 𝒔𝒊𝒎𝒊𝒍𝒂𝒓 𝒃𝒐𝒕 𝒍𝒊𝒌𝒆 𝒕𝒉𝒊𝒔, 𝒚𝒐𝒖 𝒄𝒂𝒏 𝒄𝒐𝒏𝒕𝒂𝒄𝒕 𝒕𝒉𝒆 𝒂𝒅𝒎𝒊𝒏 𝒐𝒏:/n /n  𝑷𝒉𝒐𝒏𝒆 𝒏𝒐:<code>+256704312951</code> /n /n 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑:<a href='https://wa.me/qr/A3GNDFJZU2G3P1'>𝑪𝑳𝑰𝑪𝑲 𝑯𝑬𝑹𝑬</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
